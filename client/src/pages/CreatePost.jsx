477ef4c2a715ca2091e6c1b944f706b0033c6425
SHRISTI-web <shristis308@gmail.com>
2023-02-11 21:43:34 +0530

    client side setup

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
-import { getRandomPrompt } from './utils';
+import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
